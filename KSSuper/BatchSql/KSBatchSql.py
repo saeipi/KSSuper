b8edcb0cd1e470d0a5ef577d8bb17c944598dd36
huchaojun <huchaojun@topgamesinc.com>
2020-10-10 13:25:31 +0800

    修改

--- KSSuper/BatchSql/KSBatchSql.py
+++ KSSuper/BatchSql/KSBatchSql.py
@@ -1,9 +1,5 @@
 import xlrd
 import os
-
-import sys;
-print('Python %s on %s' % (sys.version, sys.platform))
-print(sys.path)
 from Export.KSExport import KSExport
 
 class KSBatchSql(object):
@@ -26,10 +22,7 @@ class KSBatchSql(object):
             out_name = sheet.cell(int(index), 0).value
             self.export_csv(sql, str_out_path, out_name+".csv")
 
-# batch_sql = KSBatchSql()
-# str_sql_path = os.path.abspath("../Resources/SQL/job_sql.xls")
-# str_out_path = "/Users/saeipi/Desktop/jobs"
-# batch_sql.execute_sqls(str_sql_path, str_out_path)
-
-
-# sys.path.extend([WORKING_DIR_AND_PYTHON_PATHS])
+batch_sql = KSBatchSql()
+str_sql_path = os.path.abspath("../Resources/SQL/job_sql.xls")
+str_out_path = "/Users/saeipi/Desktop/jobs"
+batch_sql.execute_sqls(str_sql_path, str_out_path)
